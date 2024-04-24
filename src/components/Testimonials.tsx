c64b29c50ff5c14389865a2505f261a75a568f75
Rubens <rubensgouveia@ioasys.com.br>
2024-04-24 13:07:24 -0300

    ajuste testimonials

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,6 +1,6 @@
 export const Testimonials: React.FC = () => {
   return (
-    <section className="py-20 bg-gray-100">
+    <section className="py-20 ">
       <div className="container mx-auto px-4">
         <h2 className="text-3xl font-bold text-center">
           Depoimentos de alunos satisfeitos
