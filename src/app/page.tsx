ebf3e212b12802032d662596e43d055ab8d1f2f4
Rubens <rubensgouveia@ioasys.com.br>
2024-04-24 13:41:29 -0300

    test github actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main>
       <HeroSection />
       <AboutClasses />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <AboutStudio />
       <Footer />
     </main>
