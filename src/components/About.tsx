0fab56ccebf3566a3bd8b4499d0886f3283bf4b8
Rubens <rubensgouveia@ioasys.com.br>
2024-04-25 09:18:10 -0300

    ajustando texto do componente About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,8 +5,27 @@ export const AboutStudio: React.FC = () => {
         <h2 className="text-3xl font-bold text-center">
           Sobre o Estúdio Acima do Solado
         </h2>
-        <p className="mt-4 text-center">
-          Fundado em [ano de fundação], o Estúdio de Dança Acima do Solado...
+        <p className="mt-4 text-center max-w-screen-lg mx-auto">
+          O Acima do Solado, localizado no coração de Tambauzinho e agora também
+          com uma nova unidade sala VIP em Tambaú a partir de maio, é um estúdio
+          de dança e teatro que vai além do convencional. Aqui, a paixão pela
+          arte se funde com a alegria de viver, criando um ambiente acolhedor e
+          estimulante para o aprendizado.
+        </p>
+        <p className="mt-4 text-center max-w-screen-lg mx-auto">
+          Nossa equipe de profissionais altamente capacitados e apaixonados
+          oferece aulas dinâmicas e envolventes, que permitem aos alunos
+          desenvolver suas habilidades artísticas e descobrir o verdadeiro valor
+          da arte em suas vidas. Acreditamos no poder da dança e do teatro para
+          unir pessoas, criar laços duradouros e proporcionar momentos de
+          descontração e festa.
+        </p>
+        <p className="mt-4 text-center max-w-screen-lg mx-auto">
+          Ao se juntar a nós, você embarcará em uma jornada de autodescoberta e
+          crescimento pessoal, desenvolvendo confiança, criatividade e uma
+          paixão duradoura pelas artes. Venha conhecer o Acima do Solado e faça
+          parte de uma comunidade vibrante que valoriza a arte, a amizade e a
+          alegria de viver.
         </p>
       </div>
     </section>
