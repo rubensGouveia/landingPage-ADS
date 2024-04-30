5cd61807fbe661693652ced3a7d1480ce7a70baf
Rubens <rubensgouveia@ioasys.com.br>
2024-04-30 18:22:48 -0300

    adicionando video

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,12 @@
+"use client";
 import Link from "next/link";
+import { useLayoutEffect } from "react";
 
 export const HeroSection: React.FC = () => {
+  useLayoutEffect(() => {
+    const myVideo = document?.getElementById("video") as HTMLVideoElement;
+    if (document && myVideo) myVideo?.play();
+  }, []);
   return (
     <section className="text-center py-20 bg-blue-500 text-white">
       <div className="z-10 w-full mb-4 items-center justify-center font-mono text-sm flex ">
@@ -12,9 +18,12 @@ export const HeroSection: React.FC = () => {
         />
       </div>
 
-      <h1 className="text-4xl font-bold px-6">
+      <h1 className="text-4xl font-bold px-6 mb-8">
         Aprenda a dançar forró antes do São João
       </h1>
+      <div className="z-10 max-w-[80%] w-full mb-8 items-center justify-center font-mono text-sm flex mx-auto">
+        <video id={"video"} autoPlay controls src="/video.mp4"></video>
+      </div>
       <p className="mt-4 px-6">
         Você quer arrasar na pista durante as festas juninas? Nossas aulas de
         forró VIP são perfeitas para você!
